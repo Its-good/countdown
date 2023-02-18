02264f12cebc90b6c9396fcbb95642c152d86409
Its-good <102301094+Its-good@users.noreply.github.com>
2023-02-18 14:12:30 +0530

    Add initial countdown website files

---
+++ script.js
@@ -0,0 +1,29 @@
+var waiting=prompt("What is it for which you are waiting?");
+var waitDate=prompt("When is it?");
+document.querySelector(".waiting").innerHTML="Waiting for "+waiting;
+waitDate=new Date(waitDate);
+var waitingMonth=waitDate.getMonth();
+var waitingDate=waitDate.getDate();
+var waitingYear=waitDate.getFullYear();
+const waitMonth=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+waitingMonth=waitMonth[waitingMonth];
+var output="On "+waitingDate+" "+waitingMonth+" "+waitingYear
+document.querySelector(".waitDate").innerHTML=output;
+
+function countdown(){
+    var today=new Date();
+    var ms=waitDate-today;
+    var time=Math.floor(ms/1000);
+    var sec=time%60;
+    var mins=Math.floor((time/60)%60);
+    var hrs=Math.floor((time/3600)%24);
+    var days=Math.floor(time/3600/24);
+    document.querySelector(".seconds").innerHTML=sec;
+    document.querySelector(".mins").innerHTML=mins;
+    document.querySelector(".hrs").innerHTML=hrs;
+    document.querySelector(".days").innerHTML=days;
+}
+
+countdown();
+
+setInterval(countdown, 1000)
